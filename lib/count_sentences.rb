944b4b9b952d2422115709995dac39e9d3041778
Berkley Brown <berkleydeane@gmail.com>
2020-06-05 16:21:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences = selg.gsub(",", "142").gsub("?", "142").gsub("!", "142").split("142").reject {}
+    sentences = selg.gsub(",", "142").gsub("?", "142").gsub("!", "142").split("142").reject {|sentence|}
     
   end
 end
